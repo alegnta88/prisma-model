616830422dc9a3774058fa0ca894fa0994a94f70
alegnta88 <alegnta88@gmail.com>
2025-11-23 18:41:41 +0300

    change to postgres

--- config/env.js
+++ config/env.js
@@ -1,14 +1,26 @@
-import mongoose from 'mongoose';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD
+});
 
 const connectToDatabase = async () => {
-  if (!process.env.MONGO_URI) {
-    console.error("MongoDB connection failed: MONGO_URI is missing in environment variables.");
+  try {
+    await pool.connect();
+    console.log('Successfully connected to PostgreSQL');
+  } catch (err) {
+    console.error('PostgreSQL connection failed:', err);
     process.exit(1);
   }
+};
 
-  await mongoose.connect(process.env.MONGO_URI, {
-  });
-  console.log('Successfully connected to MongoDB');
-}
-
-export default connectToDatabase;
+export { pool, connectToDatabase };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectToDatabase from './config/env.js';
+import { connectToDatabase } from './config/env.js';
 import { connectCloudinary } from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import morgan from 'morgan';
