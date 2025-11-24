ec55eb0866a1535cf37720b724e633093721f266
alegnta88 <alegnta88@gmail.com>
2025-11-24 11:11:31 +0300

    category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,22 +1,19 @@
-import { 
-  createCategoryService, 
-  getAllCategoriesService 
-} from "../services/categoryService.js";
+import { createCategoryService, getAllCategoriesService } from '../services/categoryService.js';
 
 export const createCategory = async (req, res) => {
   try {
     const category = await createCategoryService(req.user, req.body);
     res.status(201).json({ success: true, category });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ success: false, message: error.message });
   }
 };
 
 export const getAllCategories = async (req, res) => {
   try {
     const categories = await getAllCategoriesService();
-    res.json(categories);
+    res.status(200).json({ success: true, categories });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
